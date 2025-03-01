1a223757f051021cf411ddcde3f6389a41048ffa
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-03-02 00:14:27 +0100

    Creating list of names

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
+    const lista = [
+        "imagen1.jpg",
+        "imagen2.jpg",
+        "imagen3.jpg",
+        "imagen4.jpg",
+        "imagen5.jpg",
+        "imagen6.jpg",
+        "imagen7.jpg",
+        "imagen8.jpg",
+        "imagen9.jpg",
+        "imagen10.jpg"];
+
 
 });
