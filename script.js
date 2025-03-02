afd4d14fd86661c6762b145c49325c3040a9f3ad
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-03-02 10:18:05 +0100

    Coded JS lines waiting to be tested until finding car images

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const autos = [
-        "imagen1.jpg",
-        "imagen2.jpg",
-        "imagen3.jpg",
-        "imagen4.jpg",
-        "imagen5.jpg",
-        "imagen6.jpg",
-        "imagen7.jpg",
-        "imagen8.jpg",
-        "imagen9.jpg",
-        "imagen10.jpg"
+        "auto1.jpg",
+        "auto2.jpg",
+        "auto3.jpg",
+        "auto4.jpg",
+        "auto5.jpg",
+        "auto6.jpg",
+        "auto7.jpg",
+        "auto8.jpg",
+        "auto9.jpg",
+        "auto10.jpg"
     ];
 
+    const lienzo = document.getElementById('#portrait');
+    const contenedor = document.querySelector('main section div#auto');
 
+    autos.forEach(auto => {
+        document.addEventListener('click', () => {
+            lienzo.style.display = 'none';
+            const imagen = document.createElement('img');
+            imagen.setAttribute('src', imagen);
+            imagen.setAttribute('alt', 'automóvil');
+            contenedor.appendChild(imagen);
+            // lienzo.style.display = 'block';
+        })
+    }
 });
