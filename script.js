1a7a5a634a99c788a49c4b8c4e40baa49a7e42b3
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-03-03 17:25:32 +0100

    Working to avoid repetition of index value

--- script.js
+++ script.js
@@ -22,7 +22,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     boton.addEventListener('click', () => {
 
+        // Declaring variable to validate that index doesn't repeat.
+        let previous = 0;
+
         const index = Math.floor(Math.random() * 10) // Obtaining a random number between 0 and 9 without decimals.
+
+        if (index === previous && index < 9) {
+            index++;
+        } else {
+            if (index === previous && index === 9)
+                index--;
+        }
+
         const imagen = document.createElement('img');
         imagen.setAttribute('src', autos[index], 'alt', 'automóvil');
         // imagen.setAttribute('alt', 'automóvil');
